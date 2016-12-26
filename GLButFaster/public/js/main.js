e59665c6289027651b0c0b06b23618291b9add2c
Zachary A Chapman <zeechapman@gmail.com>
2016-12-26 00:08:12 -0700

    Large commit

---
+++ GLButFaster/public/js/main.js
@@ -0,0 +1,29 @@
+﻿// Variables
+var rate = 1; // Speed of the audio
+var aud = "../media/aud/gl-";  // Specifying the path of the media of choice as a shortcut
+var lyrics = [aud + "0.ogg", aud + "1.ogg", aud + "2.ogg", aud + "3.ogg", aud + "4.ogg", aud + "5.ogg", aud + "6.ogg", aud + "7.ogg", aud + "8.ogg", aud + "9.ogg"];
+
+
+// Random number
+function ran() {
+    return Math.floor(Math.random() * 10);
+}
+
+// It's jQuery ti-- wait...
+window.onload = function () {
+    // The sound that starts
+    var sndStart = new Howl({ src: aud + "start.ogg"});
+
+    // Sections of the lyrics
+    var sndLyrics = new Howl({ src: lyrics[ran()] });
+
+    // The "middle point"
+    var sndMid = new Howl({ src: aud + "mid.ogg", rate: rate });
+
+    // The end is here...END IT
+    var sndEnd = new Howl({ src: aud + "end.ogg" });
+
+
+    sndStart.play();
+    
+};
--- GLButFaster/server.js
+++ GLButFaster/server.js
@@ -2,7 +2,17 @@
 var http = require('http');
 var port = process.env.port || 1337;
 
-http.createServer(function (req, res) {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello World\n');
-}).listen(port);
+var express = require('express');
+var howler = require('howler');
+
+var app = express();
+
+var path = require('path');
+
+// Set the paths
+app.use(express.static(__dirname + '/public'));
+app.use('/js', express.static(__dirname + '/node_modules/howler/dist/'));
+app.use('/js', express.static(__dirname + '/node_modules/video.js/dist/'));
+
+// Start the server, and use port 1337 (default value of Visual Studio.  I kept it, because I think it's cute)
+app.listen(port);
