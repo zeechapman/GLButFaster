eed6aa2190ed646579548e87eb0f22558cfa3ed5
zeechapman <zeechapman@gmail.com>
2017-01-03 10:01:50 -0700

    Changes again.  Sorry, too lazy for real message

--- GLButFaster/public/js/main.js
+++ GLButFaster/public/js/main.js
@@ -5,21 +5,25 @@
 */
 
 // Variables
-var rate = 1; // Speed of the audio
+var rate = 1; // Speed of the audio.
 var aud = "../media/aud/gl-";  // Specifying the path of the media of choice as a shortcut
 var counter = 0; // Counts how many times they tried to do their part.
 
+var wereUp = 0.04;  // Each time they get lucky, multiply the rate by this value (default 0.04)
+var imUp = 0.02;    // Each time just one of them got lucky, multiply by this value (default 0.02)
 
 var loopMax = 15; // Default value - 15
-var luckyMax = 13.5; // Default value - 13.5;
+var luckyMax = 13.5; // Default value - 13.5
+
 var colorLoop = 0; // Reserved for cycleColors()
-var colorIndex = 0; // For my pretty list of colors
+var colorIndex = -1; // For my pretty list of colors
 
 // Counts how many times they get lucky
 var luckyCounter = 0;
 
 // A list of colors.  Because I like colors
-var colors = [ "#7d24f2", "#e23f36", "#399362", "#753942", "#d1e236", "#36b2e2"];
+var colors = [ "#34005e", "#333333", "#5e5c00", "#5e2a00", "#555555"];
+
 
 var sndStart = new Howl({ src: aud + "start.ogg" }); // Start of the audio
 var sndMid = new Howl({ src: aud + "mid.ogg", rate: rate }); // The part where the song goes "We're up all night to get lucky"
@@ -36,35 +40,39 @@ var L5 = new Howl({ src: aud + "5.ogg", onend: function () { lyricsMix(); } });
 var L6 = new Howl({ src: aud + "6.ogg", onend: function () { lyricsMix(); } });
 var L7 = new Howl({ src: aud + "7.ogg", onend: function () { lyricsMix(); } });
 
+
+
 // Random number generator (remember: it starts out at 0, then counts up)
 function ran(max) {
     return Math.floor(Math.random() * max);
 }
 
+// What will be causing the background to flash colors for a short time
 function cycleColors() {
-  setTimeout(function() {
-    document.body.style.backgroundColor = colors[colorIndex];
-    colorLoop++;
-    if (colorIndex > colors.length) {
+  setTimeout(function() { // Execute this every 10 miliseconds, 30 times
+    if (colorIndex == 4) {
       colorIndex = -1; // Don't judge me
     }
+    document.body.style.backgroundColor = colors[colorIndex];
+    colorLoop++;
     colorIndex++;
-    if (colorLoop < 80) {
+    if (colorLoop < 30) {
       cycleColors();
     } else {
-        document.body.style.backgroundColor = "#000";
+        document.body.style.backgroundColor = "#000"; // After the loop is over, set the background back to black
     }
   }, 10); // Seizure warning with this one
 }
 
+// Endlessly makes them get lucky, until the maximum number of luckys they can get
 function getLucky() {
     sndMid.play();
     sndMid.rate(rate);
     sndMid.once('end', function () {  // If it plays too fast, then stop it.  If not, then keep the madness going.
       if (rate < luckyMax) {
-        document.getElementById("pic").width *= "1.01";
-        luckyCounter++;
-        getLucky();
+        document.getElementById("pic").width *= "1.01"; // For no reason, scale the logo by 1% each time
+        luckyCounter++; // Count the number of times they get lucky
+        getLucky(); // Start of recursion
       } else {
           sndEnd.play();
           vid.play();
@@ -76,6 +84,7 @@ function getLucky() {
     rate += 0.04;
 }
 
+// Play the sections of the chorus based on which random number is generated
 function lyricsMix() {
     var random = ran(8);
     if (counter <= loopMax) {
@@ -107,7 +116,7 @@ function lyricsMix() {
             case 7:
                 L7.rate(rate).play(); // One of the instances of getting lucky
                 rate += 0.01; // Since only one of them is getting lucky, speed it up partially
-                luckyCounter += 0.5; // Please refer to line 99
+                luckyCounter += 0.5; // Please refer to line 108
                 break;
             default:
                 document.getElementById("vid").insertAdjacentHTML('afterend', "<p>Ummm...something happened...you're not supposed to see this...</p>");
@@ -119,7 +128,7 @@ function lyricsMix() {
     counter++;
 }
 
-// It's jQuery ti-- wait...
+// When the document loads, begin
 window.onload = function () {
   console.log("Are you ready for a world of hurt?");
     var vid = document.getElementById("vid");
@@ -128,5 +137,10 @@ window.onload = function () {
         console.log("Time to mix things up a bit...");
         lyricsMix();
     });
+
+};
+
+
+window.onblur = function() {
 
 };
